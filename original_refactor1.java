616fbfc90ff1619f7afdcba3362ac050b9f6a344
mushfiqurgalib <48786723+mushfiqurgalib@users.noreply.github.com>
2025-07-10 22:29:23 +0600

    Update original_refactor1.java

--- original_refactor1.java
+++ original_refactor1.java
@@ -1,73 +1,52 @@
-@@ -60,14 +60,14 @@ public class SparkRepository {
-     public static final String REPOSITORY_DIR = "__spark_repository__";
-     public static final String PREFIX_ARCHIVE = "__archive_";
-     public static final String PREFIX_LIB = "__lib_";
--    public static final String SPARK_DPP = "spark-dpp";
-     public static final String SPARK_2X = "spark-2x";
--    public static final String SUFFIX = ".zip";
- 
-     private static final String PATH_DELIMITER = "/";
-     private static final String FILE_NAME_SEPARATOR = "_";
- 
--    private static final String DPP_RESOURCE = "/spark-dpp/spark-dpp.jar";
-     private static final String SPARK_RESOURCE = "/jars/spark-2x.zip";
- 
-     private String remoteRepositoryPath;
-@@ -85,7 +85,7 @@ public SparkRepository(String remoteRepositoryPath, BrokerDesc brokerDesc) {
-         this.brokerDesc = brokerDesc;
-         this.currentDppVersion = Config.spark_dpp_version;
-         this.currentArchive = new SparkArchive(getRemoteArchivePath(currentDppVersion), currentDppVersion);
--        this.localDppPath = PaloFe.DORIS_HOME_DIR + DPP_RESOURCE;
-         if (!Strings.isNullOrEmpty(Config.spark_resource_path)) {
-             this.localSpark2xPath = Config.spark_resource_path;
-         } else {
-@@ -98,7 +98,7 @@ public void prepare() throws LoadException {
+
+@@ -114,20 +113,9 @@ public static DateLiteral getRangeEnd(DateLiteral beginTime, FunctionIntervalInf
+         return null;
      }
- 
-     private void initRepository() throws LoadException {
--        LOG.info("start to init remote repository");
-         boolean needUpload = false;
-         boolean needReplace = false;
-         CHECK: {
-@@ -222,7 +222,11 @@ private void getLibraries(String remoteArchivePath, List<SparkLibrary> libraries
-             if (!fileName.startsWith(PREFIX_LIB)) {
+
+-    public static Map<String, AddPartitionClause> getNonExistPartitionAddClause(OlapTable olapTable,
+-            ArrayList<TStringLiteral> partitionValues, PartitionInfo partitionInfo, ArrayList<Long> existPartitionIds)
+             throws AnalysisException {
+-        Preconditions.checkArgument(!partitionInfo.isMultiColumnPartition(),
+-                "now dont support multi key columns in auto-partition.");
+-
+         Map<String, AddPartitionClause> result = Maps.newHashMap();
+         ArrayList<Expr> partitionExprs = partitionInfo.getPartitionExprs();
+         PartitionType partitionType = partitionInfo.getType();
+@@ -144,14 +132,6 @@ public static Map<String, AddPartitionClause> getNonExistPartitionAddClause(Olap
                  continue;
              }
--            String[] lib_arg = unWrap(PREFIX_LIB, SUFFIX, fileName).split(FILE_NAME_SEPARATOR);
-             if (lib_arg.length != 2) {
-                 continue;
+             filterPartitionValues.add(value);
+-
+-
+-            Long id = partitionInfo.contains(partitionValue, partitionType);
+-            if (id != null) { // found
+-                existPartitionIds.add(id);
+-                continue;
+-            }
+-
+             if (partitionType == PartitionType.RANGE) {
+                 String beginTime = value;
+                 DateLiteral beginDateTime = new DateLiteral(beginTime, partitionColumnType);
+@@ -167,24 +147,21 @@ public static Map<String, AddPartitionClause> getNonExistPartitionAddClause(Olap
+                 listValues.add(Collections.singletonList(lowerValue));
+                 partitionKeyDesc = PartitionKeyDesc.createIn(
+                         listValues);
+-
+                 partitionName += getFormatPartitionValue(lowerValue.getStringValue());
+                 if (partitionColumnType.isStringType()) {
+                     partitionName += "_" + System.currentTimeMillis();
+                 }
+             } else {
+-                throw new AnalysisException("auto-partition only support range and list partition");
              }
-@@ -232,16 +236,12 @@ private void getLibraries(String remoteArchivePath, List<SparkLibrary> libraries
-             }
-             String type = lib_arg[1];
-             SparkLibrary.LibType libType = null;
--            switch (type) {
--                case SPARK_DPP:
--                    libType = SparkLibrary.LibType.DPP;
--                    break;
--                case SPARK_2X:
--                    libType = SparkLibrary.LibType.SPARK2X;
--                    break;
--                default:
--                    Preconditions.checkState(false, "wrong library type: " + type);
--                    break;
-             }
-             SparkLibrary remoteFile = new SparkLibrary(fileStatus.path, md5sum, libType, fileStatus.size);
-             libraries.add(remoteFile);
-@@ -259,7 +259,7 @@ public String getMd5String(String filePath) throws LoadException {
-             LOG.debug("get md5sum from file {}, md5sum={}", filePath, md5sum);
-             return md5sum;
-         } catch (FileNotFoundException e) {
--            throw new LoadException("file " + filePath + "dose not exist");
-         } catch (IOException e) {
-             throw new LoadException("failed to get md5sum from file " + filePath);
+
+             Map<String, String> partitionProperties = Maps.newHashMap();
+             DistributionDesc distributionDesc = olapTable.getDefaultDistributionInfo().toDistributionDesc();
+
+-            SinglePartitionDesc partitionDesc = new SinglePartitionDesc(true, partitionName,
+                     partitionKeyDesc, partitionProperties);
+
+-            AddPartitionClause addPartitionClause = new AddPartitionClause(partitionDesc,
+                     distributionDesc, partitionProperties, false);
+             result.put(partitionName, addPartitionClause);
          }
-@@ -302,8 +302,11 @@ private static String getFileName(String delimiter, String path) {
-         return path.substring(path.lastIndexOf(delimiter) + 1);
-     }
- 
--    private static String unWrap(String prefix, String suffix, String fileName) {
--        return fileName.substring(prefix.length(), fileName.length() - suffix.length());
-     }
- 
-     private static String joinPrefix(String prefix, String fileName) {
