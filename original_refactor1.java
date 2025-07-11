f0e15010c9078d20d6000dd4771ad074919c0edb
mushfiqurgalib <48786723+mushfiqurgalib@users.noreply.github.com>
2025-07-11 09:25:44 +0600

    Update original_refactor1.java

--- original_refactor1.java
+++ original_refactor1.java
@@ -1,52 +1,38 @@
-
-@@ -114,20 +113,9 @@ public static DateLiteral getRangeEnd(DateLiteral beginTime, FunctionIntervalInf
-         return null;
+@@ -127,7 +127,7 @@ public class StatisticsUtil {
+     public static List<ResultRow> executeQuery(String template, Map<String, String> params) {
+         StringSubstitutor stringSubstitutor = new StringSubstitutor(params);
+         String sql = stringSubstitutor.replace(template);
+-        return execStatisticQuery(sql);
      }
-
--    public static Map<String, AddPartitionClause> getNonExistPartitionAddClause(OlapTable olapTable,
--            ArrayList<TStringLiteral> partitionValues, PartitionInfo partitionInfo, ArrayList<Long> existPartitionIds)
-             throws AnalysisException {
--        Preconditions.checkArgument(!partitionInfo.isMultiColumnPartition(),
--                "now dont support multi key columns in auto-partition.");
--
-         Map<String, AddPartitionClause> result = Maps.newHashMap();
-         ArrayList<Expr> partitionExprs = partitionInfo.getPartitionExprs();
-         PartitionType partitionType = partitionInfo.getType();
-@@ -144,14 +132,6 @@ public static Map<String, AddPartitionClause> getNonExistPartitionAddClause(Olap
-                 continue;
-             }
-             filterPartitionValues.add(value);
--
--
--            Long id = partitionInfo.contains(partitionValue, partitionType);
--            if (id != null) { // found
--                existPartitionIds.add(id);
--                continue;
--            }
--
-             if (partitionType == PartitionType.RANGE) {
-                 String beginTime = value;
-                 DateLiteral beginDateTime = new DateLiteral(beginTime, partitionColumnType);
-@@ -167,24 +147,21 @@ public static Map<String, AddPartitionClause> getNonExistPartitionAddClause(Olap
-                 listValues.add(Collections.singletonList(lowerValue));
-                 partitionKeyDesc = PartitionKeyDesc.createIn(
-                         listValues);
--
-                 partitionName += getFormatPartitionValue(lowerValue.getStringValue());
-                 if (partitionColumnType.isStringType()) {
-                     partitionName += "_" + System.currentTimeMillis();
-                 }
-             } else {
--                throw new AnalysisException("auto-partition only support range and list partition");
-             }
-
-             Map<String, String> partitionProperties = Maps.newHashMap();
-             DistributionDesc distributionDesc = olapTable.getDefaultDistributionInfo().toDistributionDesc();
-
--            SinglePartitionDesc partitionDesc = new SinglePartitionDesc(true, partitionName,
-                     partitionKeyDesc, partitionProperties);
-
--            AddPartitionClause addPartitionClause = new AddPartitionClause(partitionDesc,
-                     distributionDesc, partitionProperties, false);
-             result.put(partitionName, addPartitionClause);
+ 
+     public static void execUpdate(String template, Map<String, String> params) throws Exception {
+@@ -137,10 +137,15 @@ public static void execUpdate(String template, Map<String, String> params) throw
+     }
+ 
+     public static List<ResultRow> execStatisticQuery(String sql) {
+         if (!FeConstants.enableInternalSchemaDb) {
+             return Collections.emptyList();
          }
+-        try (AutoCloseConnectContext r = StatisticsUtil.buildConnectContext()) {
+             if (Config.isCloudMode()) {
+                 r.connectContext.getCloudCluster();
+             }
+@@ -178,10 +183,10 @@ public static List<Histogram> deserializeToHistogramStatistics(List<ResultRow> r
+     }
+ 
+     public static AutoCloseConnectContext buildConnectContext() {
+-        return buildConnectContext(false);
+     }
+ 
+-    public static AutoCloseConnectContext buildConnectContext(boolean limitScan) {
+         ConnectContext connectContext = new ConnectContext();
+         SessionVariable sessionVariable = connectContext.getSessionVariable();
+         sessionVariable.internalSession = true;
+@@ -207,7 +212,14 @@ public static AutoCloseConnectContext buildConnectContext(boolean limitScan) {
+         connectContext.setQualifiedUser(UserIdentity.ROOT.getQualifiedUser());
+         connectContext.setCurrentUserIdentity(UserIdentity.ROOT);
+         connectContext.setStartTime();
+-        return new AutoCloseConnectContext(connectContext);
+     }
+ 
+     public static void analyze(StatementBase statementBase) throws UserException {
