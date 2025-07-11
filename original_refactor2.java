176be254307f93bbc52df1a21c705cd6b390553c
mushfiqurgalib <48786723+mushfiqurgalib@users.noreply.github.com>
2025-07-11 09:29:37 +0600

    Update original_refactor2.java

--- original_refactor2.java
+++ original_refactor2.java
@@ -1,55 +1,60 @@
-public class UdfUtils {
-     public static final Unsafe UNSAFE;
-     private static final long UNSAFE_COPY_THRESHOLD = 1024L * 1024L;
-     public static final long BYTE_ARRAY_OFFSET;
-@@ -59,6 +66,93 @@ public class UdfUtils {
-         BYTE_ARRAY_OFFSET = UNSAFE.arrayBaseOffset(byte[].class);
+@@ -74,8 +74,11 @@ public class NereidsSqlCacheManager {
+     // value: SqlCacheContext
+     private volatile Cache<String, SqlCacheContext> sqlCaches;
+ 
+-    public NereidsSqlCacheManager(int sqlCacheNum, long cacheIntervalSeconds) {
+-        sqlCaches = buildSqlCaches(sqlCacheNum, cacheIntervalSeconds);
      }
  
-     protected static Pair<Type, Integer> fromThrift(TTypeDesc typeDesc, int nodeIdx) throws InternalException {
-         TTypeNode node = typeDesc.getTypes().get(nodeIdx);
-         Type type = null;
-@@ -134,14 +228,16 @@ public static Pair<Boolean, JavaUdfDataType> setReturnType(Type retType, Class<?
-         if (!JavaUdfDataType.isSupported(retType)) {
-             throw new InternalException("Unsupported return type: " + retType.toSql());
+     public static synchronized void updateConfig() {
+@@ -90,22 +93,24 @@ public static synchronized void updateConfig() {
+ 
+         Cache<String, SqlCacheContext> sqlCaches = buildSqlCaches(
+                 Config.sql_cache_manage_num,
+-                Config.cache_last_version_interval_second
+         );
+         sqlCaches.putAll(sqlCacheManager.sqlCaches.asMap());
+         sqlCacheManager.sqlCaches = sqlCaches;
+     }
+ 
+-    private static Cache<String, SqlCacheContext> buildSqlCaches(int sqlCacheNum, long cacheIntervalSeconds) {
+-        sqlCacheNum = sqlCacheNum < 0 ? 100 : sqlCacheNum;
+-        cacheIntervalSeconds = cacheIntervalSeconds < 0 ? 30 : cacheIntervalSeconds;
+-
+-        return Caffeine.newBuilder()
+-                .maximumSize(sqlCacheNum)
+-                .expireAfterAccess(Duration.ofSeconds(cacheIntervalSeconds))
+                 // auto evict cache when jvm memory too low
+-                .softValues()
+-                .build();
+     }
+ 
+     /** tryAddFeCache */
+@@ -237,9 +242,6 @@ public Optional<LogicalSqlCache> tryParseSql(ConnectContext connectContext, Stri
+     }
+ 
+     private boolean tablesOrDataChanged(Env env, SqlCacheContext sqlCacheContext) {
+-        long latestPartitionTime = sqlCacheContext.getLatestPartitionTime();
+-        long latestPartitionVersion = sqlCacheContext.getLatestPartitionVersion();
+-
+         if (sqlCacheContext.hasUnsupportedTables()) {
+             return true;
          }
--        JavaUdfDataType javaType = JavaUdfDataType.getType(udfReturnType);
--        if (retType.getPrimitiveType().toThrift() != javaType.getPrimitiveType()) {
--            return Pair.of(false, javaType);
-         }
--        return Pair.of(true, javaType);
-     }
- 
--    public static LocalDateTime convertToDateTime(long date) {
-         int year = (int) (date >> 48);
-         int yearMonth = (int) (date >> 40);
-         int yearMonthDay = (int) (date >> 32);
-@@ -181,29 +307,149 @@ public static LocalDateTime convertToDateTime(long date) {
-         int second = (minuteTypeNeg >> 4);
-         //here don't need those bits are type = ((minus_type_neg >> 1) & 0x7);
- 
--        LocalDateTime value = LocalDateTime.of(year, month, day, hour, minute, second);
-         return value;
-     }
- 
-
--    public static LocalDate convertToDate(long date) {
-         int year = (int) (date >> 48);
-         int yearMonth = (int) (date >> 40);
-         int yearMonthDay = (int) (date >> 32);
- 
-         int month = (yearMonth & 0XFF);
-         int day = (yearMonthDay & 0XFF);
--        LocalDate value = LocalDate.of(year, month, day);
-         return value;
-     }
- 
- 
--    public static long convertDateTimeToLong(int year, int month, int day, int hour, int minute, int second,
-             boolean isDate) {
-         long time = 0;
-         time = time + year;
-@@ -219,6 +465,48 @@ public static long convertDateTimeToLong(int year, int month, int day, int hour,
-         return time;
-     }
- 
+@@ -255,7 +257,7 @@ private boolean tablesOrDataChanged(Env env, SqlCacheContext sqlCacheContext) {
+             long cacheTableTime = scanTable.latestTimestamp;
+             long currentTableVersion = olapTable.getVisibleVersion();
+             long cacheTableVersion = scanTable.latestVersion;
+-            // some partitions have been dropped, or delete or update or insert rows into new partition?
+             if (currentTableTime > cacheTableTime
+                     || (currentTableTime == cacheTableTime && currentTableVersion > cacheTableVersion)) {
+                 return true;
+@@ -264,9 +266,7 @@ private boolean tablesOrDataChanged(Env env, SqlCacheContext sqlCacheContext) {
+             for (Long scanPartitionId: scanTable.getScanPartitions()) {
+                 Partition partition = olapTable.getPartition(scanPartitionId);
+                 // partition == null: is this partition truncated?
+-                if (partition == null || partition.getVisibleVersionTime() > latestPartitionTime
+-                        || (partition.getVisibleVersionTime() == latestPartitionTime
+-                        && partition.getVisibleVersion() > latestPartitionVersion)) {
+                     return true;
+                 }
+             }
